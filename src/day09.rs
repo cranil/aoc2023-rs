1da43f103c00dc9bba3354650b23b4efbd13567d
Anil Raghuramu <contact@cranil.io>
2023-12-09 14:19:06 +0000

    day 9 remove recursion

--- src/day09.rs
+++ src/day09.rs
@@ -1,4 +1,7 @@
-use crate::utils::{main, test};
+use crate::{
+    algos::Grid,
+    utils::{main, test},
+};
 
 fn get_contents(filename: &str) -> Vec<Vec<i64>> {
     let lines = crate::utils::read_lines(filename);
@@ -13,17 +16,32 @@ fn get_contents(filename: &str) -> Vec<Vec<i64>> {
 }
 
 fn newton_extrapolation(y: &Vec<i64>) -> i64 {
-    let y0 = y[0];
-    if y.iter().skip(1).all(|&x| x == y0) {
-        return y0;
+    let mut g = Grid::new(y.len(), y.len());
+    for i in 0..y.len() {
+        g.set(0, i, y[i]);
     }
-    let n = y.len();
-    let mut yp = vec![0; n - 1];
-    for i in 0..n - 1 {
-        yp[i] = y[i + 1] - y[i];
+    for i in 1..y.len() {
+        let mut pval = g.at(i - 1, 1).unwrap() - g.at(i - 1, 0).unwrap();
+        let mut eq = true;
+        g.set(i, 0, pval);
+
+        for j in 1..y.len() - i {
+            let val = g.at(i - 1, j + 1).unwrap() - g.at(i - 1, j).unwrap();
+            g.set(i, j, val);
+            if val != pval {
+                eq = false;
+            }
+            pval = val;
+        }
+        if eq {
+            break;
+        }
     }
-    let ypn = newton_extrapolation(&yp);
-    return ypn + y.last().unwrap();
+    let mut sum = 0;
+    for i in 0..y.len() {
+        sum += g.at(y.len() - i - 1, i).unwrap();
+    }
+    return sum;
 }
 
 fn part1(input: &Vec<Vec<i64>>) -> i64 {
