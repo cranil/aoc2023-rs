6687655e7f30e6e4a54a53f4e8e6a3f23e15c32d
Anil Raghuramu <contact@cranil.io>
2023-12-11 04:27:43 +0000

    day 10 efficiency update

--- src/day10.rs
+++ src/day10.rs
@@ -20,46 +20,52 @@ fn get_contents(filename: &str) -> (Grid<char>, (usize, usize)) {
     return (grid, start);
 }
 
+fn cmap(c: &char) -> char {
+    let p = match c {
+        'S' => '*',
+        'L' => '╚',
+        'F' => '╔',
+        '7' => '╗',
+        'J' => '╝',
+        '|' => '║',
+        '-' => '═',
+        '.' => '■',
+        _ => ' ',
+    };
+    return p;
+}
+
 fn pretty_print_cycle(
     grid: &Grid<char>,
     cycle: &Vec<(usize, usize)>,
     interior_points: &Vec<(usize, usize)>,
 ) {
-    let mut pretty_grid = Grid::new(grid.width + 2, grid.height + 2);
-    for x in 0..grid.width {
-        for y in 0..grid.height {
+    for _ in 0..grid.width + 2 {
+        print!("█");
+    }
+    println!("");
+    for y in 0..grid.height {
+        print!("█");
+        for x in 0..grid.width {
             let c = grid.at(x, y).unwrap();
+            let p = cmap(c);
             if cycle.contains(&(x, y)) {
-                let p = match c {
-                    'S' => '*',
-                    'L' => '└',
-                    'F' => '┌',
-                    '7' => '┐',
-                    'J' => '┘',
-                    '|' => '│',
-                    '-' => '─',
-                    '.' => '.',
-                    _ => ' ',
-                };
-                pretty_grid.set(x + 1, y + 1, p);
+                print!("\x1b[0;36m{}\x1b[0m", p);
             } else {
-                pretty_grid.set(x + 1, y + 1, '.');
-            }
-            if interior_points.contains(&(x, y)) {
-                pretty_grid.set(x + 1, y + 1, '█');
+                if interior_points.contains(&(x, y)) {
+                    print!("\x1b[0;32m{}\x1b[0m", p);
+                } else {
+                    print!("\x1b[0;31m{}\x1b[0m", p);
+                }
             }
         }
+        print!("█");
+        println!("");
     }
-    for x in 0..grid.width + 2 {
-        pretty_grid.set(x, 0, '█');
-        pretty_grid.set(x, grid.height + 1, '█');
+    for _ in 0..grid.width + 2 {
+        print!("█");
     }
-    for y in 0..grid.height + 2 {
-        pretty_grid.set(0, y, '█');
-        pretty_grid.set(grid.width + 1, y, '█');
-    }
-
-    println!("{}", pretty_grid);
+    println!("");
 }
 
 fn find_cycle(grid: &Grid<char>, start: &(usize, usize)) -> Vec<Vec<(usize, usize)>> {
@@ -81,17 +87,21 @@ fn find_cycle(grid: &Grid<char>, start: &(usize, usize)) -> Vec<Vec<(usize, usiz
 
     let mut cycles = Vec::new();
     for (di, dj) in vec![(0, 1), (0, -1), (1, 0), (-1, 0)].iter() {
+        let mut debug = Grid::new(grid.width, grid.height);
+        for x in 0..grid.width {
+            for y in 0..grid.height {
+                debug.set(x, y, '.');
+            }
+        }
+        debug.set(start.0, start.1, '*');
         let mut visited = Grid::new(grid.width, grid.height);
-        visited.set(start.0, start.1, true);
-
-        let (ni, nj) = (start.0 as isize + di, start.1 as isize + dj);
-        if ni < 0 || nj < 0 || ni >= grid.height as isize || nj >= grid.width as isize {
+        let (nx, ny) = (start.0 as isize + di, start.1 as isize + dj);
+        if nx < 0 || ny < 0 || nx >= grid.width as isize || ny >= grid.height as isize {
             continue;
         }
-        let (ni, nj) = (ni as usize, nj as usize);
+        let (ni, nj) = (nx as usize, ny as usize);
         let mut stack = VecDeque::new();
         let mut cycle = Vec::new();
-        cycle.push((start.0, start.1));
         stack.push_front((ni, nj));
         // dfs
         while !stack.is_empty() {
@@ -100,8 +110,10 @@ fn find_cycle(grid: &Grid<char>, start: &(usize, usize)) -> Vec<Vec<(usize, usiz
                 continue;
             }
             visited.set(x, y, true);
-            match grid.at(x, y).unwrap() {
+            let c = grid.at(x, y).unwrap();
+            match c {
                 'S' => {
+                    cycle.push((x, y));
                     break;
                 }
                 'L' => {
@@ -125,7 +137,6 @@ fn find_cycle(grid: &Grid<char>, start: &(usize, usize)) -> Vec<Vec<(usize, usiz
                     let down = grid.at(x, y + 1);
                     if let Some(right) = right {
                         if !connections_h.get(&'F').unwrap().contains(&right) {
-                            println!("right: {:?}, {:?}", right, connections_h.get(&'F').unwrap());
                             break;
                         }
                         stack.push_front((x + 1, y));
@@ -224,10 +235,14 @@ fn find_cycle(grid: &Grid<char>, start: &(usize, usize)) -> Vec<Vec<(usize, usiz
                     panic!("Invalid char");
                 }
             }
+            debug.set(x, y, cmap(c));
+            // println!("{}", debug);
             cycle.push((x, y));
         }
         if cycle.len() > 1 {
-            cycles.push(cycle);
+            if is_neighbor(cycle[0], cycle[cycle.len() - 1]) {
+                cycles.push(cycle);
+            }
         }
     }
     return cycles;
@@ -241,27 +256,40 @@ fn part1(grid: &(Grid<char>, (usize, usize))) -> i64 {
     return ((cycle.len() + 1) / 2) as i64;
 }
 
+fn is_neighbor(p0: (usize, usize), p1: (usize, usize)) -> bool {
+    let (x0, y0) = p0;
+    let (x1, y1) = p1;
+    if x0 == x1 {
+        return y0 == y1 + 1 || y0 == y1 - 1;
+    }
+    if y0 == y1 {
+        return x0 == x1 + 1 || x0 == x1 - 1;
+    }
+    return false;
+}
+
 fn find_interior_points(grid: &Grid<char>, cycle: &Vec<(usize, usize)>) -> Vec<(usize, usize)> {
     let mut interior_points = Vec::new();
-    for x in 0..grid.width {
-        for y in 0..grid.height {
+    for y in 0..grid.height {
+        let cycle_y = cycle.iter().filter(|(_, yp)| y == *yp).collect::<Vec<_>>();
+        for x in 0..grid.width {
             let mut count0 = 0;
 
-            if cycle.contains(&(x, y)) {
+            if *grid.at(x, y).unwrap() != '.' {
                 continue;
             }
-            let mut xt = x as i64;
-            while xt < grid.width as i64 {
-                xt += 1;
-                if cycle.contains(&(xt as usize, y)) {
-                    match grid.at(xt as usize, y).unwrap() {
+            cycle_y
+                .iter()
+                .filter(|(xp, _)| *xp >= x)
+                .for_each(|(xp, _)| {
+                    match grid.at(*xp, y).unwrap() {
+                        // count saddles as interections (eg. F---J, L---7) and ignore tangents (eg. L---J, F---7)
                         '7' | 'F' | '-' => {}
                         _ => {
                             count0 += 1;
                         }
                     }
-                }
-            }
+                });
             if count0 % 2 == 1 {
                 interior_points.push((x, y));
             }
@@ -271,19 +299,61 @@ fn find_interior_points(grid: &Grid<char>, cycle: &Vec<(usize, usize)>) -> Vec<(
 }
 
 fn part2(grid: &(Grid<char>, (usize, usize))) -> i64 {
-    let (grid, start) = grid;
-    let mut cycles = find_cycle(&grid, start);
+    let (grid_o, start) = grid;
+    let mut cycles = find_cycle(&grid_o, start);
     cycles.sort_by(|a, b| a.len().cmp(&b.len()));
     let cycle = cycles.last().unwrap();
+
+    let mut grid = Grid::new(grid_o.width, grid_o.height);
+    for x in 0..grid.width {
+        for y in 0..grid.height {
+            grid.set(x, y, '.');
+        }
+    }
+
+    for (x, y) in cycle {
+        grid.set(*x, *y, *grid_o.at(*x, *y).unwrap());
+    }
+
+    let (x0, y0) = start;
+    let (x1, y1) = cycle[0];
+    let (dx0, dy0) = (x1 as isize - *x0 as isize, y1 as isize - *y0 as isize);
+    let (x1, y1) = cycle[cycle.len() - 2];
+    let (dx1, dy1) = (x1 as isize - *x0 as isize, y1 as isize - *y0 as isize);
+
+    match (dx0, dy0, dx1, dy1) {
+        (0, 1, 1, 0) | (1, 0, 0, 1) => {
+            grid.set(start.0, start.1, 'F');
+        }
+        (0, -1, 1, 0) | (1, 0, 0, -1) => {
+            grid.set(start.0, start.1, 'L');
+        }
+        (0, 1, -1, 0) | (-1, 0, 0, 1) => {
+            grid.set(start.0, start.1, 'J');
+        }
+        (0, -1, -1, 0) | (-1, 0, 0, -1) => {
+            grid.set(start.0, start.1, '7');
+        }
+        (1, 0, -1, 0) | (-1, 0, 1, 0) => {
+            grid.set(start.0, start.1, '-');
+        }
+        (0, 1, 0, -1) | (0, -1, 0, 1) => {
+            grid.set(start.0, start.1, '|');
+        }
+        _ => {
+            panic!("Invalid cycle");
+        }
+    }
+
     let interior_points = find_interior_points(&grid, cycle);
-    // pretty_print_cycle(&grid, &cycle, &interior_points);
+    // pretty_print_cycle(&grid_o, &cycle, &interior_points);
     return interior_points.len() as i64;
 }
 
 #[cfg(test)]
 mod consts {
     pub const PART1_INPUTS: [(&str, i64); 1] = [("test_inputs/day10/test01.txt", 8)];
-    pub const PART2_INPUTS: [(&str, i64); 0] = [];
+    pub const PART2_INPUTS: [(&str, i64); 1] = [("test_inputs/day10/test02.txt", 8)];
 }
 
 test!();
