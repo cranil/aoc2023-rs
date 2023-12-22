5f52597a7a74116614dea9bbcdd2ed0892522cb9
Anil Raghuramu <contact@cranil.io>
2023-12-22 08:06:04 +0000

    day 21 and 22

--- src/day21.rs
+++ src/day21.rs
@@ -1,15 +1,153 @@
-use crate::utils::{main, test};
+use std::{collections::HashSet, mem::swap};
 
-fn get_contents(filename: &str) -> &str {
-    return filename;
+use crate::{
+    grid::Grid,
+    utils::{main, read_lines, test},
+};
+
+type InputData = (Grid<char>, (usize, usize));
+
+fn get_contents(filename: &str) -> InputData {
+    let lines = read_lines(filename);
+    let mut start = (0, 0);
+    let mut grid = Grid::new(lines[0].len(), lines.len());
+    for (y, line) in lines.iter().enumerate() {
+        for (x, c) in line.chars().enumerate() {
+            grid.set(x, y, c);
+            if c == 'S' {
+                start = (x, y);
+                grid.set(x, y, '.');
+            }
+        }
+    }
+    return (grid, start);
 }
 
-fn part1(_input_file: &str) -> i64 {
-    panic!("{} part 1 not implemented", std::module_path!());
+fn get_neighbours(x: isize, y: isize) -> [(isize, isize); 4] {
+    [(x - 1, y), (x + 1, y), (x, y - 1), (x, y + 1)]
 }
 
-fn part2(_input_file: &str) -> i64 {
-    panic!("{} part 1 not implemented", std::module_path!());
+fn part1((grid, start): &InputData) -> i64 {
+    let start = (start.0 as isize, start.1 as isize);
+    let mut frontier = HashSet::<(isize, isize)>::new();
+    let mut expanded = HashSet::<(isize, isize)>::new();
+    let steps = 50;
+    expanded.insert(start.clone());
+    for _ in 0..steps {
+        let mut new_expanded = HashSet::new();
+        frontier.extend(expanded.iter());
+        for pos in expanded.iter() {
+            let neighbours = get_neighbours(pos.0 as isize, pos.1 as isize);
+            for neighbour in neighbours.iter() {
+                if expanded.contains(neighbour) {
+                    continue;
+                }
+                let Some(c) = grid.at(neighbour.0 as usize, neighbour.1 as usize) else {
+                    continue;
+                };
+                if *c == '.' {
+                    new_expanded.insert(neighbour.clone());
+                }
+            }
+        }
+        expanded = new_expanded;
+    }
+    expanded.len() as i64
+}
+
+fn part2((grid, start): &InputData) -> i64 {
+    assert!(grid.width == grid.height);
+
+    let start = (start.0 as isize, start.1 as isize);
+    let mut curr = HashSet::new();
+    let mut next = HashSet::new();
+    let mut steps = 0;
+
+    let mut expansion1 = 0;
+    let mut expansion2 = 0;
+    let mut _expansion3 = 0;
+
+    let mut steps1 = 0;
+    let mut steps2 = 0;
+
+    curr.insert(start.clone());
+    loop {
+        // let mut next = HashSet::new();
+        for pos in curr.iter() {
+            let neighbours = get_neighbours(pos.0 as isize, pos.1 as isize);
+            for neighbour in neighbours.iter() {
+                let mut x = neighbour.0 + start.0;
+                let mut y = neighbour.1 + start.1;
+                if curr.contains(neighbour) {
+                    continue;
+                }
+
+                if x < 0 {
+                    let m = x / grid.width as isize;
+                    x += (-m + 1) * grid.width as isize;
+                }
+                if y < 0 {
+                    let m = y / grid.height as isize;
+                    y += (-m + 1) * grid.height as isize;
+                }
+                x %= grid.width as isize;
+                y %= grid.height as isize;
+
+                if let Some(c) = grid.at(x as usize, y as usize) {
+                    if *c == '.' {
+                        next.insert(neighbour.clone());
+                    }
+                } else {
+                    println!("{} {}", x, y);
+                    panic!();
+                }
+            }
+        }
+        swap(&mut curr, &mut next);
+        next.clear();
+        steps += 1;
+
+        let mut max_x = 0;
+        let mut max_y = 0;
+        let mut min_x = 0;
+        let mut min_y = 0;
+
+        for pos in curr.iter() {
+            max_x = max_x.max(pos.0);
+            max_y = max_y.max(pos.1);
+            min_x = min_x.min(pos.0);
+            min_y = min_y.min(pos.1);
+        }
+
+        let w = (max_x - min_x + 1) as usize;
+        let h = (max_y - min_y + 1) as usize;
+
+        if w == 3 * grid.width && h == 3 * grid.height {
+            expansion1 = curr.len() as i64;
+            steps1 = steps;
+        }
+        if w == 5 * grid.width && h == 5 * grid.height {
+            expansion2 = curr.len() as i64;
+            steps2 = steps;
+        }
+        if w == 7 * grid.width && h == 7 * grid.height {
+            _expansion3 = curr.len() as i64;
+            break;
+        }
+    }
+
+    // only works for input.txt from the problem :(
+    let a = (_expansion3 - 2 * expansion2 + expansion1) / 2;
+    let b = (expansion2 - expansion1) - 3 * a;
+    let c = expansion1 - a - b;
+
+    let steps = 26501365;
+
+    let m = steps2 - steps1;
+    let n = steps1 - m;
+
+    let k = (steps - n) / m;
+    return a * k * k + b * k + c;
 }
 
 #[cfg(test)]
--- src/day22.rs
+++ src/day22.rs
@@ -1,15 +1,186 @@
-use crate::utils::{main, test};
+use std::collections::{HashSet, VecDeque};
 
-fn get_contents(filename: &str) -> &str {
-    return filename;
+use crate::{
+    grid::Grid,
+    utils::{main, read_lines, test},
+};
+
+#[derive(Clone, Debug)]
+struct Brick {
+    from: (isize, isize, isize),
+    to: (isize, isize, isize),
 }
 
-fn part1(_input_file: &str) -> i64 {
-    panic!("{} part 1 not implemented", std::module_path!());
+type InputData = Vec<Brick>;
+
+fn get_contents(filename: &str) -> InputData {
+    read_lines(filename)
+        .iter()
+        .map(|line| {
+            let mut parts = line.split('~');
+            let from = parts
+                .next()
+                .unwrap()
+                .split(',')
+                .map(|s| s.parse::<isize>().unwrap())
+                .collect::<Vec<_>>();
+            let to = parts
+                .next()
+                .unwrap()
+                .split(',')
+                .map(|s| s.parse::<isize>().unwrap())
+                .collect::<Vec<_>>();
+            Brick {
+                from: (from[0], from[1], from[2]),
+                to: (to[0], to[1], to[2]),
+            }
+        })
+        .collect()
 }
 
-fn part2(_input_file: &str) -> i64 {
-    panic!("{} part 1 not implemented", std::module_path!());
+fn get_blocks(brick: &Brick) -> Vec<(isize, isize, isize)> {
+    let mut blocks = Vec::new();
+    for z in brick.from.2..=brick.to.2 {
+        for y in brick.from.1..=brick.to.1 {
+            for x in brick.from.0..=brick.to.0 {
+                blocks.push((x, y, z));
+            }
+        }
+    }
+    blocks
+}
+
+fn get_z(brick_num: usize, brick: &Brick, grid: &Vec<Grid<usize>>) -> isize {
+    let mut z = 0;
+    let blocks = get_blocks(brick);
+    for zp in (1..brick.from.2 as usize).rev() {
+        let mut all_free = true;
+        for (x, y, _) in blocks.iter() {
+            if let Some(c) = grid[zp].at(*x as usize, *y as usize) {
+                if *c != 0 || *c == brick_num + 1 {
+                    all_free = false;
+                    break;
+                }
+            }
+        }
+        if !all_free {
+            break;
+        }
+        z = zp as isize;
+    }
+    if z == 0 {
+        z = blocks.iter().map(|(_, _, z)| *z).min().unwrap();
+    }
+    z
+}
+
+fn find_supports(bricks: &InputData) -> (Vec<HashSet<usize>>, Vec<HashSet<usize>>) {
+    let mut bricks = bricks.clone();
+    let mut x_max = 0;
+    let mut y_max = 0;
+    let mut z_max = 0;
+
+    let mut x_min = isize::MAX;
+    let mut y_min = isize::MAX;
+    let mut z_min = isize::MAX;
+
+    for brick in bricks.iter() {
+        x_max = x_max.max(brick.from.0).max(brick.to.0);
+        y_max = y_max.max(brick.from.1).max(brick.to.1);
+        z_max = z_max.max(brick.from.2).max(brick.to.2);
+
+        x_min = x_min.min(brick.from.0).min(brick.to.0);
+        y_min = y_min.min(brick.from.1).min(brick.to.1);
+        z_min = z_min.min(brick.from.2).min(brick.to.2);
+    }
+
+    let mut grid = vec![Grid::new(1 + x_max as usize, 1 + y_max as usize); 1 + z_max as usize];
+
+    let mut sorted_map = (0..bricks.len()).collect::<Vec<_>>();
+    sorted_map.sort_by_key(|i| bricks[*i].from.2);
+
+    for i in sorted_map.iter() {
+        let brick = &mut bricks[*i];
+        let zp = get_z(*i, brick, &grid);
+        let blocks = get_blocks(brick);
+
+        let zdiff = brick.from.2 - zp;
+        for (x, y, z) in blocks.iter() {
+            grid[(z - zdiff) as usize].set(*x as usize, *y as usize, *i + 1);
+        }
+
+        brick.to.2 -= zdiff;
+        brick.from.2 -= zdiff;
+    }
+
+    let mut supports = vec![HashSet::new(); bricks.len()];
+    let mut supported_by = vec![HashSet::new(); bricks.len()];
+    for z in 0..z_max as usize {
+        let curr = &grid[z];
+        let above = &grid[z + 1];
+        for y in 0..=y_max as usize {
+            for x in 0..=x_max as usize {
+                let curr_pt = curr.at(x, y).unwrap();
+                let above_pt = above.at(x, y).unwrap();
+                if *curr_pt == 0 {
+                    continue;
+                }
+                if *above_pt == 0 || *above_pt == *curr_pt {
+                    continue;
+                }
+                supports[*curr_pt as usize - 1].insert(*above_pt);
+                supported_by[*above_pt as usize - 1].insert(*curr_pt);
+            }
+        }
+    }
+    (supports, supported_by)
+}
+
+fn part1(bricks: &InputData) -> i64 {
+    let (supports, supported_by) = find_supports(bricks);
+    let mut can_disintegrate = 0;
+    for (i, _) in bricks.iter().enumerate() {
+        let mut all_supported = true;
+        for brick_above in supports[i].iter() {
+            if supported_by[*brick_above as usize - 1].len() == 1 {
+                all_supported = false;
+                break;
+            }
+        }
+        if all_supported {
+            can_disintegrate += 1;
+        }
+    }
+    can_disintegrate as i64
+}
+
+fn part2(bricks: &InputData) -> i64 {
+    let (supports, supported_by) = find_supports(bricks);
+
+    let mut affects = vec![0; bricks.len()];
+    for i in 0..bricks.len() {
+        let mut affected = HashSet::new();
+        let mut queue = VecDeque::new();
+        queue.push_back(i);
+        affected.insert(i);
+        while let Some(brick_idx) = queue.pop_front() {
+            for brick_above in supports[brick_idx].iter() {
+                let mut topples = true;
+                for supp in supported_by[*brick_above as usize - 1].iter() {
+                    if !affected.contains(&(*supp - 1)) {
+                        topples = false;
+                        break;
+                    }
+                }
+                if topples {
+                    queue.push_back(*brick_above - 1);
+                    affected.insert(*brick_above - 1);
+                }
+            }
+        }
+        affects[i] = affected.len() - 1;
+    }
+    affects.iter().sum::<usize>() as i64
 }
 
 #[cfg(test)]
